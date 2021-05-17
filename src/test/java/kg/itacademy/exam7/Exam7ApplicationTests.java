2f42a0f0e81f9468f7d6de0ad9e573a47c544020
RDP <elizachurokova@gmail.com>
2021-05-17 23:02:38 +0600

    exam7 initial

---
+++ src/test/java/kg/itacademy/exam7/Exam7ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.itacademy.exam7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
